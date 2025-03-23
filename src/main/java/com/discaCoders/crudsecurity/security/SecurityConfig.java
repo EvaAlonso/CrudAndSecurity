ec0e187ac06dad993dbc58bb02f5fc19101d4162
RKGoat029-dev <rkgoat029.dev@gmail.com>
2025-03-23 16:08:54 +0100

    Security finalised

--- src/main/java/com/discaCoders/crudsecurity/security/SecurityConfig.java
+++ src/main/java/com/discaCoders/crudsecurity/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.discaCoders.crudsecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +13,17 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(configurer ->
               configurer
-                      .
+                      .requestMatchers(HttpMethod.GET, "api/workers").hasAnyRole("humanResources", "workBoss")
+                      .requestMatchers(HttpMethod.GET, "api/workers/**").hasAnyRole("humanResources", "workBoss")
+                      .requestMatchers(HttpMethod.POST, "api/workers").hasRole("humanResources")
+                      .requestMatchers(HttpMethod.PUT, "api/workers").hasRole("humanResources")
+                      .requestMatchers(HttpMethod.DELETE, "api/workers/**").hasRole("humanResources")
         );
+
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
+
     }
 }
