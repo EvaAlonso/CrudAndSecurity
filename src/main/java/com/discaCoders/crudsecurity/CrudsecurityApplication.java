18ff6bb8f6428bedec9e1fa5aae8047af49d021f
Eva Alonso Contreras <Eva.Alonso@proton.me>
2025-03-23 15:03:12 +0100

    first commit, and add entity

---
+++ src/main/java/com/discaCoders/crudsecurity/CrudsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.discaCoders.crudsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudsecurityApplication.class, args);
+	}
+
+}
